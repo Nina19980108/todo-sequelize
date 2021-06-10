ebc37e8ae1a765d05a4afc427d3059af06dce7c4
Nina19980108 <ninagirl8787@gmail.com>
2021-06-10 14:35:18 +0800

    feat: set experss-handlebar

--- app.js
+++ app.js
@@ -7,13 +7,33 @@ const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main' }, { extname: '.hbs' }))
-app.set('view exgine', 'hbs')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
 })
 
 app.listen(PORT, () => {
