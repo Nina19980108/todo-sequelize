1179e79f12d316946ca13f5c4620e1520105ddb6
Nina19980108 <ninagirl8787@gmail.com>
2021-06-11 15:21:46 +0800

    feat: add passport config

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
 const bodyParser = require('body-parser')
+const passport = require('passport')
 
 const app = express()
 const PORT = 3000
@@ -11,10 +13,19 @@ const db = require('./models')
 const Todo = db.Todo
 const User = db.User
 
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({
+  secret: 'ThisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+const usePassport = require('./config/passport')
+usePassport(app)
 
 app.get('/', (req, res) => {
   return Todo.findAll({
@@ -29,9 +40,10 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
+app.post('/users/login', passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/users/login'
+}))
 
 app.get('/users/register', (req, res) => {
   res.render('register')
---
+++ config/passport.js
@@ -0,0 +1,34 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+module.exports = app => {
+  app.use(passport.initialize())
+  app.use(passport.session())
+  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (!user) {
+          return done(null, false, { message: 'That email is not registered!' })
+        }
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
+            return done(null, false, { message: 'Email or Password incorrect.' })
+          }
+          return done(null, user)
+        })
+      })
+      .catch(err => done(err, false))
+  }))
+  passport.serializeUser((user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser((id, done) => {
+    User.findByPk(id)
+      .then((user) => {
+        user = user.toJSON()
+        done(null, user)
+      }).catch(err => done(err, null))
+  })
+}
