f8adb4fc5d7e957eb99f70d6efb7e25d8776b9c6
Nina19980108 <ninagirl8787@gmail.com>
2021-06-11 16:25:06 +0800

    feat: add todo edit page and feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -24,26 +24,25 @@ router.get('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// router.get('/:id/edit', (req, res) => {
-//   const id = req.params.id
-//   return Todo.findById(id)
-//     .lean()
-//     .then(todo => res.render('edit', { todo }))
-//     .catch(error => console.log(error))
-// })
+router.get('/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
 
-// router.put('/:id', (req, res) => {
-//   const id = req.params.id
-//   const { name, isDone } = req.body
-//   return Todo.findById(id)
-//     .then(todo => {
-//       todo.name = name
-//       todo.isDone = isDone === 'on'
-//       return todo.save()
-//     })
-//     .then(() => res.redirect(`/todos/${id}`))
-//     .catch(error => console.log(error))
-// })
+router.put('/:id', (req, res) => {
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findByPk(id)
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.log(error))
+})
 
 // router.delete('/:id', (req, res) => {
 //   const id = req.params.id
