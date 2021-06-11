b3ccd554fce652c5aa0b2057113aced09e8755e2
Nina19980108 <ninagirl8787@gmail.com>
2021-06-11 16:32:27 +0800

    feat: add delete todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -44,12 +44,12 @@ router.put('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
-// router.delete('/:id', (req, res) => {
-//   const id = req.params.id
-//   return Todo.findById(id)
-//     .then(todo => todo.remove())
-//     .then(() => res.redirect('/'))
-//     .catch(error => console.log(error))
-// })
+router.delete('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => todo.destroy())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 
 module.exports = router
