6dc12eeb5187030413aacc652f5fa57184361dbb
Nina19980108 <ninagirl8787@gmail.com>
2021-06-11 11:30:46 +0800

    feat: add sequelize database

---
+++ migrations/20210611020042-add-userId-to-todo.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Todos', 'UserId', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      reference: {
+        model: 'Users',
+        key: 'id'
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Todos', 'UserId')
+  }
+};
