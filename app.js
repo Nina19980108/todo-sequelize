98f8359d58a4f06aeb4a69ba950741315659abad
Nina19980108 <ninagirl8787@gmail.com>
2021-06-12 10:02:33 +0800

    feat: add isAuthenticated to show different template

--- app.js
+++ app.js
@@ -27,6 +27,11 @@ app.use(methodOverride('_method'))
 const usePassport = require('./config/passport')
 usePassport(app)
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 const routes = require('./routes')
 app.use(routes)
 
